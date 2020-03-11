49cad49652fe2709d95109844de3fc9cdeb84e33
Renan Carlos <renancarlosgr@hotmail.com>
2020-03-11 16:22:47 -0300

    Projeto criado

---
+++ src/main/java/com/renan/pedidosComAmazonWebService/PedidosComAmazonWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.renan.pedidosComAmazonWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosComAmazonWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosComAmazonWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renan/pedidosComAmazonWebService/PedidosComAmazonWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.pedidosComAmazonWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosComAmazonWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
